3fbde4b2f619cd20c18454e152aaa171981fe289
shreyamn <155551506+shreyamn@users.noreply.github.com>
2025-02-03 17:11:27 +0530

    Update App.js

--- Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
+++ Commit-to-Cloud-github-and-hosting-simplyfied-main/src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Your Name</p> 
-          <p><strong>USN:</strong> Your USN</p>
+          <p><strong>Name:</strong> Shreya M N</p> 
+          <p><strong>USN:</strong> Your 1RVU23CSE441</p>
         </div>
       </header>
     </div>
